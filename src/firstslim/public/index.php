8c46bd288ef1944d372efe61a65dd425c2395b86
Shigehiro IDANI <idani@hirotae.com>
2020-10-13 09:39:06 +0900

    追加

---
+++ src/firstslim/public/index.php
@@ -0,0 +1,13 @@
+<?php
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Slim\Factory\AppFactory;
+
+require_once ($_SERVER['DOCUMENT_ROOT'] . '/firstslim/vendor/autoload.php');
+
+$app = AppFactory::create();
+$app->get('/firstslim/public/hello', function (ServerRequestInterface $request, ResponseInterface $response, array $args):ResponseInterface {
+    print('Hello World!');
+    return $response;
+});
