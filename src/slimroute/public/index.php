f5ecf462f561b69ee55595f54e19bd0b19a32953
Shigehiro IDANI <idani@hirotae.com>
2020-10-13 11:38:32 +0900

    P.83 getQueryParamsを追加

--- src/slimroute/public/index.php
+++ src/slimroute/public/index.php
@@ -21,4 +21,15 @@ $app->post('/slimroute/public/showParams', function (ServerRequestInterface $req
    return $response;
 });
 
+/**
+ * https://localhost/slimroute/public/showParams?name=%E4%BA%95%E8%B0%B7&age=23
+ */
+$app->get('/slimroute/public/showParams', function (ServerRequestInterface $request, ResponseInterface $response, array $args):ResponseInterface {
+    $postParams = $request->getQueryParams();
+    $name = $postParams['name'];
+    $age = $postParams['age'];
+    print(sprintf('送信されたパラメータ:名前は、%sで年齢が%s', $name, $age));
+    return $response;
+});
+
 $app->run();
