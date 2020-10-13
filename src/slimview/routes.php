4b5e4fcbf16207bf041278bff2f36532bf2e57ca
Shigehiro IDANI <idani@hirotae.com>
2020-10-13 13:58:25 +0900

    P.110 Json Response

--- src/slimview/routes.php
+++ src/slimview/routes.php
@@ -5,3 +5,17 @@ use Psr\Http\Message\ServerRequestInterface;
 
 $app->setBasePath('/slimview/public');
 
+$app->any('/getDataByJson/{id}', function (ServerRequestInterface $request, ResponseInterface $response, array $args):ResponseInterface{
+    $id = $args['id'];
+    $jsonArray = [
+        'id' => $id,
+        'name' => '井谷',
+        'age' => 36
+    ];
+    $jsonData = json_encode($jsonArray);
+    $responseBody = $response->getBody();
+    $responseBody->write($jsonData);
+    $response = $response->withHeader('Content-Type', 'application/json');
+
+    return $response;
+});
