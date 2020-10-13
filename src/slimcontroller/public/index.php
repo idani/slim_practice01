39299236a37c162cf45ff540e32e47203cd81d3a
Shigehiro IDANI <idani@hirotae.com>
2020-10-13 16:59:49 +0900

    P.174初期ファイルの登録

---
+++ src/slimcontroller/public/index.php
@@ -0,0 +1,11 @@
+<?php
+
+
+use Slim\Factory\AppFactory;
+
+require_once($_SERVER['DOCUMENT_ROOT'] . '/slimcontainer/vendor/autoload.php');
+require_once($_SERVER['DOCUMENT_ROOT'] . '/slimcontainer/containerSetups.php');
+
+$app = AppFactory::create();
+require_once($_SERVER['DOCUMENT_ROOT'] . '/slimcontainer/routes.php');
+$app->run();
