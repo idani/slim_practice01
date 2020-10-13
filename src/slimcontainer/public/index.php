4b6e3a1c340d96cc8ae3937cf2a944b6f33c5beb
Shigehiro IDANI <idani@hirotae.com>
2020-10-13 15:34:53 +0900

    P.121 コンテナ初学

--- src/slimcontainer/public/index.php
+++ src/slimcontainer/public/index.php
@@ -4,6 +4,8 @@
 use Slim\Factory\AppFactory;
 
 require_once($_SERVER['DOCUMENT_ROOT'] . '/slimcontainer/vendor/autoload.php');
+require_once($_SERVER['DOCUMENT_ROOT'] . '/slimcontainer/containerSetups.php');
+
 $app = AppFactory::create();
 require_once($_SERVER['DOCUMENT_ROOT'] . '/slimcontainer/routes.php');
 $app->run();
--- src/slimcontainer/routes.php
+++ src/slimcontainer/routes.php
@@ -6,3 +6,10 @@ use Slim\Views\Twig;
 
 $app->setBasePath('/slimcontainer/public');
 
+$app->any('/helloWithContainer', function (ServerRequestInterface $request, ResponseInterface $response, array $args):ResponseInterface{
+   $assign['name'] = 'コンテナ';
+   $twig = $this->get('view');
+   $response = $twig->render($response, 'helloWithVals.html', $assign);
+
+   return $response;
+});
