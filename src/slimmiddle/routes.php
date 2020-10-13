5cde0539fb8dfebc047bf3117367738271e12ad6
Shigehiro IDANI <idani@hirotae.com>
2020-10-13 19:16:53 +0900

    P.200 ミドルウェア

--- src/slimmiddle/routes.php
+++ src/slimmiddle/routes.php
@@ -10,5 +10,8 @@ $app->setBasePath('/slimmiddle/public');
 $app->any('/doRecordIPAddress', SlimMiddleController::class.':middlewareTest')
     ->add(new RecordIPAddress())
     ->add(new RecordIPAddressBefore())
+    ;
+
+$app->get('/beforeAndAfter', SlimMiddleController::class.':middlewareTest')
     ->add(new BeforeAndAfter())
     ;
