ee4925991c3ae1e80296400065b3b83d337fce15
Shigehiro IDANI <idani@hirotae.com>
2020-10-13 19:14:33 +0900

    P.196 前処理のミドルウェアを登録

--- src/slimmiddle/routes.php
+++ src/slimmiddle/routes.php
@@ -6,4 +6,6 @@ use SocymSlim\SlimMiddle\middlewares\RecordIPAddressBefore;
 
 $app->setBasePath('/slimmiddle/public');
 
-$app->any('/doRecordIPAddress', SlimMiddleController::class.':middlewareTest')->add(new RecordIPAddress());
+$app->any('/doRecordIPAddress', SlimMiddleController::class.':middlewareTest')
+    ->add(new RecordIPAddress())
+    ->add(new RecordIPAddressBefore());
