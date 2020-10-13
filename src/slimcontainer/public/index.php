345b4ed4cd2a32f0b5253ea41c81f1c7fd89d3eb
Shigehiro IDANI <idani@hirotae.com>
2020-10-13 14:54:39 +0900

    初期ファイルを登録

---
+++ src/slimcontainer/public/index.php
@@ -0,0 +1,9 @@
+<?php
+
+
+use Slim\Factory\AppFactory;
+
+require_once($_SERVER['DOCUMENT_ROOT'] . '/slimcontainer/vendor/autoload.php');
+$app = AppFactory::create();
+require_once($_SERVER['DOCUMENT_ROOT'] . '/slimcontainer/routes.php');
+$app->run();
---
+++ src/slimcontainer/routes.php
@@ -0,0 +1,8 @@
+<?php
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Slim\Views\Twig;
+
+$app->setBasePath('/slimcontainer/public');
+
